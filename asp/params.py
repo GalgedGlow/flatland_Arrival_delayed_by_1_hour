21d320b470dee6652e39897f74bbb560200a3018
GalgedGlow <xraphaelklugex@gmail.com>
2024-11-10 16:09:15 +0100

    created repo
    Cloned flatland repo; added own encoding and used it as primary in config file

---
+++ asp/params.py
@@ -0,0 +1,3 @@
+#primary=['asp/graph_based/actions.lp','asp/graph_based/graph.lp','asp/graph_based/traverse.lp']
+primary=['asp/own_encoding/railsched.lp']
+secondary=['asp/graph_based/actions.lp','asp/graph_based/graph.lp','asp/graph_based/traverse.lp']
